8546e2503ca230074ea76e9c8eb7cad4b8dee651
Alexander Gottweis <stablestud@protonmail.com>
2023-09-09 00:49:43 +0200

    Add zoxide to chezmoi add use it in all shells
    This commit adds zoxide (autojump) to the dotfiles which will allow
    the user to jump quickly to visited places.
    
    Also shell integration is implemented so all shells should be
    able to use zoxide. The command is `j` just as in autojump.
    Implemented shells: `bash`, `fish` and fallback `posix` which
    works with `dash` and should also work with `zsh`

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
 	fish_add_path --prepend --path "{$HOME}/bin" "{$HOME}/.local/bin"
+
+	# Initialize zoxide if in $PATH
+	if type --query zoxide
+		zoxide init --cmd=j fish | source
+	end
 end
