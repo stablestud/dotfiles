8d6b65e08ea8d3dda34996c66bad87192dc6e946
Alexander Gottweis <stablestud@protonmail.com>
2024-01-07 13:32:02 +0100

    Make ble.sh zoxide aware by loading ble.sh's zoxide integration
    This commit makes sure that the zoxide integration for ble.sh is loaded
    if zoxide is availabe.
    To be honest, I'm not quite sure what the zoxide integration does,
    but I guess it improves the `zi` interactive intergration via `fzf` with
    ble.sh

--- exact_private_dot_bashrc.d/80-zoxide.bash
+++ exact_private_dot_bashrc.d/80-zoxide.bash
@@ -3,6 +3,10 @@
 # Initialize zoxide if in $PATH
 if command -v zoxide 1>/dev/null 2>&1; then
 	eval "$(zoxide init --cmd=j bash)"
+	# Make ble.sh Zoxide aware, load its integration
+	if [ -n "${BLE_VERSION}" ]; then
+		ble-import integration/zoxide
+	fi
 fi
 
 # vim: filetype=bash
