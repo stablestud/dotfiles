b57ac822b2aedc358f843b2a80216e1ee7c6bb8e
Alexander Gottweis <stablestud@protonmail.com>
2026-07-09 23:57:28 +0200

    Fix Bash prompt when Git is not installed
    Check for the `git` command before invoking Git prompt helpers.
    This prevents `command-not-found` from running on every prompt when Git is
    not installed, avoiding messages such as:
    
    ```
    Command 'git' not found, but can be installed with:
    apt install git
    ```

--- exact_private_dot_bashrc.vendor.d/11-git-prompt-setup.bash
+++ exact_private_dot_bashrc.vendor.d/11-git-prompt-setup.bash
@@ -11,7 +11,7 @@ GIT_PS1_SHOWCOLORHINTS=1
 # If not, it can be found here:
 # https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 if [[ $(LC_ALL=C type -t __git_ps1) == "function" ]]; then
-	git_ps1='$(__git_ps1)'
+	git_ps1='$(command -v git &>/dev/null && __git_ps1)'
 fi
 
 # vim: filetype=bash
